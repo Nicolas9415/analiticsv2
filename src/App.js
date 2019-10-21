842e7569deb1bbc3fb4cb89c6ba2f85a537c4ea0
Nicolas Simmonds <Nicolas.Simmonds@endava.com>
2019-10-21 10:10:11 -0500

    bars

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import moment from "moment";
 import Map from "./Maps/Maps";
 import db from "./Analytics";
 import "./styles/map.css";
@@ -10,6 +11,9 @@ import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import { makeStyles } from "@material-ui/core/styles";
 import PieChart from "react-minimal-pie-chart";
+import Chart from "react-google-charts";
+
+console.log(moment);
 
 const useStyles = makeStyles({
   root: {
@@ -32,11 +36,23 @@ const useStyles2 = makeStyles({
 function App() {
   const [rutas, setRutas] = useState([]);
   const [, setUsuarios] = useState();
-  const [puntosInteres, setPuntosInteres] = useState();
+  const [, setPuntosInteres] = useState();
+  const [bar, setBar] = useState([]);
+  const [bar2, setBar2] = useState([]);
 
+  const days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+  ];
   useEffect(() => {
+    //RFYcvR2DtSN4KfFV9F7vnjTbupE3
     db.collection("rutas")
-      .doc("RFYcvR2DtSN4KfFV9F7vnjTbupE3")
+      .doc("667oybZ1kGlTwwzfDGFV")
       .collection("rutas")
       .get()
       .then(e => {
@@ -58,20 +74,68 @@ function App() {
       });
   }, []);
 
-  let coordsPOI;
-  if (puntosInteres) {
-    coordsPOI = puntosInteres.map(points => points["coordenadas"]);
+  useEffect(() => {
+    const data = [0, 0, 0, 0, 0, 0, 0];
+    if (rutas) {
+      rutas.forEach(time => {
+        const day = moment(time.timeStamp).weekday();
+        data[day] += 1;
+      });
+    }
+    const newData = [["Element", "User/weekDay", { role: "style" }]];
+    for (let i = 0; i < data.length; i++) {
+      newData[i + 1] = [days[i], data[i], getRandomColor()];
+    }
+    setBar(newData);
+  }, [rutas]);
+
+  useEffect(() => {
+    const data = [0, 0, 0, 0, 0, 0, 0];
+    if (rutas) {
+      rutas.forEach(data => {
+        const day = moment(data.timeStamp).weekday();
+        if (data.calorias) data[day] += data.calorias;
+      });
+    }
+    console.log(data);
+    const newData = [["Element", "User/weekDay", { role: "style" }]];
+    for (let i = 0; i < data.length; i++) {
+      newData[i + 1] = [days[i], data[i], getRandomColor()];
+    }
+    setBar2(newData);
+    console.log(newData);
+  }, [rutas]);
+
+  let coordsStart;
+  if (rutas) {
+    coordsStart = rutas.map(rute => [
+      rute["latitudInicio"],
+      rute["longitudInicio"]
+    ]);
+  }
+  let coordsFinal;
+  if (rutas) {
+    coordsFinal = rutas.map(rute => [
+      rute["latitudFinal"],
+      rute["longitudFinal"]
+    ]);
   }
 
   function getAvgDistance() {
     let a = 0;
-    if (rutas) rutas.forEach(element => (a += element.duracion));
-    const ret = (a / rutas.length / 60).toFixed(1);
+    if (rutas)
+      rutas.forEach(element => {
+        if (element.duracion) a += element.duracion;
+      });
+    const ret = (a / rutas.length).toFixed(1);
     return isNaN(ret) ? 0 : ret;
   }
   function getCalories() {
     let a = 0;
-    if (rutas) rutas.forEach(element => (a += element.calorias));
+    if (rutas)
+      rutas.forEach(element => {
+        if (element.calorias) a += element.calorias;
+      });
     const ret = (a / rutas.length).toFixed(2);
     return isNaN(ret) ? 0 : ret;
   }
@@ -108,13 +172,15 @@ function App() {
   const classes = useStyles();
   const classes2 = useStyles2();
 
-  return rutas.length > 0 ? (
+  return (
     <div className="wrapper">
-      <div className="content">
-        <div className="title">Heat Map points of interest</div>
-        <Map coords={coordsPOI} className="map" />
-      </div>
-
+      {rutas.length > 0 && (
+        <Map
+          coordsInicio={coordsStart}
+          coordsFinal={coordsFinal}
+          className="map"
+        />
+      )}
       <Paper className={classes2.root}>
         <span className="tabletitle">Averages of different statistics</span>
         <Table className={classes.root}>
@@ -150,8 +216,23 @@ function App() {
           data={getStops()}
         />
       </div>
+      <div className="content">
+        <div className="title">Avg cyclist per day of week</div>
+
+        <Chart chartType="ColumnChart" width="100%" height="400px" data={bar} />
+      </div>
+      <div className="content">
+        <div className="title">Avg cyclist per day of week</div>
+
+        <Chart
+          chartType="ColumnChart"
+          width="100%"
+          height="400px"
+          data={bar2}
+        />
+      </div>
     </div>
-  ) : null;
+  );
 }
 
 export default App;
--- src/Maps/Maps.js
+++ src/Maps/Maps.js
@@ -1,50 +1,86 @@
-import React, { useRef } from "react";
+/* eslint-disable react/prop-types */
+import React, { useRef, useEffect } from "react";
 import GoogleMapsApiLoader from "google-maps-api-loader";
+import "../styles/map.css";
 
 //const openGeocoder = require("node-open-geocoder");
 
 export default function Map(props) {
-  const { coords } = props;
+  const { coordsInicio, coordsFinal } = props;
 
   const ref = useRef(null);
+  const ref2 = useRef(null);
 
-  GoogleMapsApiLoader({
-    libraries: ["places", "visualization"],
-    apiKey: "AIzaSyCqMkRlzo-OHKohU-0ovuLBbNvt6O4wczQ" // optional
-  }).then(
-    function(google) {
-      let fullCoords;
-      let start;
-      let heatmapData = [];
+  useEffect(() => {
+    GoogleMapsApiLoader({
+      libraries: ["places", "visualization"],
+      apiKey: "AIzaSyCqMkRlzo-OHKohU-0ovuLBbNvt6O4wczQ" // optional
+    }).then(
+      function(google) {
+        let start;
+        let heatmapData = [];
+        let start2;
+        let heatmapData2 = [];
 
-      fullCoords = coords.map(e => [e.latitude, e.longitude]);
+        coordsInicio.forEach(coord => {
+          if (coord[0] && coord[1])
+            heatmapData.push(new google.maps.LatLng(coord[0], coord[1]));
+        });
+        coordsFinal.forEach(coord => {
+          if (coord[0] && coord[1])
+            heatmapData2.push(new google.maps.LatLng(coord[0], coord[1]));
+        });
+        start = new google.maps.LatLng(coordsInicio[0][0], coordsInicio[0][1]);
+        start2 = new google.maps.LatLng(coordsFinal[0][0], coordsFinal[0][1]);
 
-      fullCoords.map(coord =>
-        heatmapData.push(new google.maps.LatLng(coord[0], coord[1]))
-      );
+        const map = new google.maps.Map(ref.current, {
+          center: start,
+          zoom: 5,
+          mapTypeId: "roadmap"
+        });
+        const heatmap = new google.maps.visualization.HeatmapLayer({
+          data: heatmapData,
+          radius: 16
+        });
 
-      start = new google.maps.LatLng(fullCoords[0][0], fullCoords[0][1]);
+        const map2 = new google.maps.Map(ref2.current, {
+          center: start2,
+          zoom: 5,
+          mapTypeId: "roadmap"
+        });
 
-      const map = new google.maps.Map(ref.current, {
-        center: start,
-        zoom: 14,
-        mapTypeId: "roadmap"
-      });
+        const heatmap2 = new google.maps.visualization.HeatmapLayer({
+          data: heatmapData2,
+          radius: 16
+        });
 
-      var heatmap = new google.maps.visualization.HeatmapLayer({
-        data: heatmapData,
-        radius: 25
-      });
-      heatmap.setMap(map);
-    },
-    function(err) {
-      console.error(err);
-    }
-  );
+        heatmap.setMap(map);
+        heatmap2.setMap(map2);
+      },
+      function(err) {
+        console.error(err);
+      }
+    );
+    // eslint-disable-next-line
+  }, []);
+
   return (
-    <div
-      ref={ref}
-      style={{ height: "50vh", width: "30vw", margin: "10px 0" }}
-    ></div>
+    <React.Fragment>
+      <div className="content">
+        <div className="title">Heat Map most common start points</div>
+        <div
+          ref={ref}
+          style={{ height: "50vh", width: "30vw", margin: "10px 0" }}
+        ></div>
+      </div>
+
+      <div className="content">
+        <div className="title">Heat Map most common start points</div>
+        <div
+          ref={ref2}
+          style={{ height: "50vh", width: "30vw", margin: "10px 0" }}
+        ></div>
+      </div>
+    </React.Fragment>
   );
 }
